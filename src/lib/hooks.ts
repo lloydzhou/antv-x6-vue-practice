ef46e41060fd3d2777aad7d26fc50a1514ef96c8
lloydzhou <lloydzhou@qq.com>
2023-02-18 15:35:02 +0800

    using ref remove reactive

--- src/lib/hooks.ts
+++ src/lib/hooks.ts
@@ -1,34 +1,33 @@
 import type { Graph, Node, Edge } from '@antv/x6'
-import { watch, toRefs, onMounted, shallowReactive } from 'vue'
+import { watch, onMounted, shallowRef } from 'vue'
 import { diffCells, patch, checkId } from './utils'
 
 type GraphState = {nodes?: Node.Metadata[], edges?: Edge.Metadata[], graph?: Graph}
 
 export const useGraphState = (initState: GraphState = {}) => {
-  const { nodes = [], edges = [], graph } = initState;
-  const state = shallowReactive({
-    nodes,
-    edges,
-    graph
-  })
-  const methods = {
-    setGraph: (g: Graph) => g && (state.graph = g),
-    // state数据之前先检查id是否存在，自动创建id，确保diffCells的时候能使用id进行判断
-    setNodes: (_nodes: Node.Metadata[]) => state.nodes = _nodes.map(checkId),
-    setEdges: (_edges: Edge.Metadata[]) => state.edges = _edges.map(checkId),
-  }
+  const nodes = shallowRef(initState.nodes || [])
+  const edges = shallowRef(initState.edges || [])
+  const graph = shallowRef(initState.graph || null)
+  const setGraph = (g: Graph) => g && (graph.value = g)
+  // 设置数据之前先检查id是否存在，自动创建id，确保diffCells的时候能使用id进行判断
+  const setNodes = (_nodes: Node.Metadata[]) => nodes.value = _nodes.map(checkId)
+  const setEdges = (_edges: Edge.Metadata[]) => edges.value = _edges.map(checkId)
 
   // 先使用diffCells拿到变化数据，再使用patch函数更新数据到x6画布
   watch(() => ({
-    nodes: diffCells(state.graph, state.nodes, 'node'),
-    edges: diffCells(state.graph, state.edges, 'edge'),
+    nodes: diffCells(graph.value, nodes.value, 'node'),
+    edges: diffCells(graph.value, edges.value, 'edge'),
   }), ({nodes, edges}) => {
-    patch(state.graph, nodes)
-    patch(state.graph, edges)
+    patch(graph.value, nodes)
+    patch(graph.value, edges)
   })
 
   return {
-    ...toRefs(state),
-    ...methods,
+    nodes,
+    edges,
+    graph,
+    setNodes,
+    setEdges,
+    setGraph
   }
 };
